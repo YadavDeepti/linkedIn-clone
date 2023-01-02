99bfc3d2a485fcb7a999b12a31d8a19e196462b5
Deepti Yadav <deeptiyadav940@gmail.com>
2023-01-03 00:15:48 +0530

    input section done

--- src/Feed.js
+++ src/Feed.js
@@ -1,11 +1,32 @@
-import './Sidebar.css'
+import './Feed.css'
+import InputOption from './inputOption';
+import React from 'react';
+import CreateIcon from '@mui/icons-material/Create';
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 
-import React from 'react'
+
 
 const Feed = () => {
   return (
     <div className='feed'>
-      feed section
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" />
+            <button type='submit'>Send</button>
+          </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title='Photos' color='#70B5F9'/>
+          <InputOption Icon={SubscriptionsIcon} title='Video' color='#e7a33e'/>
+          <InputOption Icon={EventNoteIcon} title='Event' color='#c0cbcd'/>
+          <InputOption Icon={CalendarViewDayIcon} title='Write article' color='#7fc15e'/>
+        </div>
+      </div>
     </div>
   )
 }
